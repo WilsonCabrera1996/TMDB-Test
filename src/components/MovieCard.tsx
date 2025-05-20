4e13f0fb8e6f5ebf7a4bbbde0e3d5c551a5513f3
Daviddotcoms <fortniterenegeraider@gmail.com>
2025-05-20 16:32:54 -0500

    Add movie link into card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import type { Movie } from '../types/Movie';
-
+import { Link } from 'react-router-dom';
 
 interface Props {
-    movie:Movie;
+    movie: Movie;
 }
 
-const MovieCard: React.FC<Props> =({ movie }) => {
+const MovieCard: React.FC<Props> = ({ movie }) => {
     return (
-        <div style ={{width: '200px', margin:'1rem'}}>
-            <img 
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-            style={{ width : '100%', borderRadius:'10px'}}
-            />
-            <h3>{movie.title}</h3>
-            <p><small>{movie.release_date}</small></p>
-        </div>
+        <Link to={`/movies/${movie.id}`} style={{ color: "white", margin: 8 }}>
+            <div style={{ width: '180px', margin: '1rem' }}>
+                <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                    style={{ width: '100%', borderRadius: '10px' }}
+                />
+                <h3>{movie.title}</h3>
+                <p><small>{movie.release_date}</small></p>
+            </div>
+        </Link>
     );
 };
 
