2c82eb1352f2bde733ff28fd2d776701df6b7632
Daviddotcoms <fortniterenegeraider@gmail.com>
2025-05-20 16:31:01 -0500

    Create getMovieById function

--- src/services/MovieServices.ts
+++ src/services/MovieServices.ts
@@ -1,10 +1,27 @@
-// services/MovieService.ts
+import type { Movie } from "../types/Movie";
+
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 
+function sleep(time: number) {
+  return new Promise(res => 
+    setTimeout(() => {
+      res(true)
+    }, time)
+  )
+}
+
 export const getPopularMovies = async () => {
-  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=bde250ce834a6987890c1c2c31128dd1&language=es-ES&page=1`);
+  await sleep(1000)
+  const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=es-ES&page=1`);
   const data = await res.json();
   return data.results;
+};
+
+export const getMovieById = async (id: string): Promise<Movie> => {
+  await sleep(1000)
+  const res = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=es-ES&page=1`);
+  const data = await res.json();
+  return data;
 };
