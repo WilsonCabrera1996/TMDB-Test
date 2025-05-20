5bc594a076b88cd41c4241b6fe9b7469afc4fb57
Daviddotcoms <fortniterenegeraider@gmail.com>
2025-05-20 16:43:09 -0500

    Add ta

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { FavoritesProvider } from './context/FavoritesContext.tsx';
 import './index.css'
 import App from './App.tsx'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
-<React.StrictMode>
-   <BrowserRouter>
-    <FavoritesProvider>
-        <App/>
-    </FavoritesProvider>
-   </BrowserRouter>
-</React.StrictMode>
+
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools />
+            <BrowserRouter>
+                <FavoritesProvider>
+                    <App />
+                </FavoritesProvider>
+            </BrowserRouter>
+        </QueryClientProvider>
+    </React.StrictMode>
 );
