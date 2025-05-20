3bc3b7e53aa3561c07b7e88244dab677612103e9
Daviddotcoms <fortniterenegeraider@gmail.com>
2025-05-20 17:26:47 -0500

    Set image shadow & library icons

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,21 +1,24 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useMovie } from '../hooks/useMovie';
 
 const MovieDetails: React.FC = () => {
   const { id } = useParams();
   const { movieQuery } = useMovie(id ? id : "")
+  const navigation = useNavigate()
 
   if (movieQuery.isLoading || !movieQuery.data) return (
     <div style={{ display: "flex", justifyContent: "center", marginTop: 24 }}>
       <p>Cargando peliculas...</p>
     </div>
   )
+
   return (
-    <div style={{ padding: '2rem', display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+    <div style={{ padding: '2rem', display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", position: "relative" }}>
+      <button style={{ position: "absolute", top: 24, left: 110 }} onClick={() => navigation(-1)}>{"<- Volver atrás"}</button>
       <h2>{movieQuery.data.title}</h2>
       <div style={{ display: "flex", gap: 32, paddingRight: 24, paddingLeft: 24, alignItems: "center", marginBottom: 12 }}>
-        <img src={`https://image.tmdb.org/t/p/w500${movieQuery.data.poster_path}`} alt={movieQuery.data.title} width={350} />
-        <p style={{ width: "60ch" }}>{movieQuery.data.overview}</p>
+        <img src={`https://image.tmdb.org/t/p/w500${movieQuery.data.poster_path}`} alt={movieQuery.data.title} width={350} style={{ borderRadius: 16, boxShadow: "0 1px 1px rgba(0,0,0,0.08), 0 2px 2px rgba(0,0,0,0.12), 0 8px 8px rgba(0,0,0,0.16), 0 24px 24px rgba(0,0,0,0.20)" }} />
+        <p style={{ width: "60ch" }}>{movieQuery.data.overview ? movieQuery.data.overview : "Esta película no tiene descripción"}</p>
       </div>
       <p><strong>Fecha de lanzamiento:</strong> {movieQuery.data.release_date}</p>
     </div>
--- src/services/MovieServices.ts
+++ src/services/MovieServices.ts
@@ -4,23 +4,13 @@ const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-function sleep(time: number) {
-  return new Promise(res => 
-    setTimeout(() => {
-      res(true)
-    }, time)
-  )
-}
-
 export const getPopularMovies = async () => {
-  await sleep(1000)
   const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=es-ES&page=1`);
   const data = await res.json();
   return data.results;
 };
 
 export const getMovieById = async (id: string): Promise<Movie> => {
-  await sleep(1000)
   const res = await fetch(`${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=es-ES&page=1`);
   const data = await res.json();
   return data;
