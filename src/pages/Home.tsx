e0a08bb2fe595314f437188316b8976a91942ce8
WilsonCabrera1996 <cabrerawilson22@gmail.com>
2025-05-22 10:37:03 -0500

    Revert "Updated home"
    This reverts commit b06ebae0bcebcf4faab14ba156ca2670791712ba.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,10 @@
-import { Link } from 'react-router-dom';
+
 
 const Home = () => {
     return (
-        <div className="hero-background">
-            <div className="welcome-text">
-                <h1>Bienvenido a Cine APP</h1>
-            </div>
-            
+        <div style={{ textAlign: 'center', marginTop: '2rem', flex: 1, minWidth: "100%" }}>
+            <h1>Bienvenido a Cine APP</h1>
             <p>Explora peliculas populares,descubre tus favoritas y mas.</p>
-
-            <div>
-            <Link to="/movies">
-                 <button>Explorar</button>
-             </Link>
-            </div>
         </div>
     );
 };
